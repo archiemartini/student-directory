36923e28e2f81e882a8b67bd73427d6eec2ddd9d
EC2 Default User <ec2-user@ip-172-31-33-101.ec2.internal>
2022-04-12 16:49:22 +0000

    Save function implemented

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-students = [ 
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 @students = []
 
 def input_students
@@ -48,6 +34,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "7. Save"
     puts "9. Exit"
 end
 
@@ -63,6 +50,8 @@ def interactive_menu
         input_students
         when "2"
         show_students
+        when "7"
+        save_students
         when "9"
         exit
         else
@@ -75,6 +64,17 @@ def show_students
     print_header
     print_student_list(@students)
     print_footer(@students)
+end
+
+def save_students
+    
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
 end
 
 # students = input_students

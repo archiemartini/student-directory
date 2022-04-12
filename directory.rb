9af699d41b5efd7c57aca107558da65e4af6020f
EC2 Default User <ec2-user@ip-172-31-33-101.ec2.internal>
2022-04-12 13:23:57 +0000

    Added interactive menu and a global instance variable 'students'

--- directory.rb
+++ directory.rb
@@ -12,21 +12,22 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 
+@students = []
+
 def input_students
     
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     
     name = gets.chomp
-    students = []
     
     while !name.empty? do
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
     
-    students
+    @students
 end
 
 def print_header 
@@ -34,9 +35,9 @@ def print_header
     puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-      puts "#{name[:name]} (#{name[:cohort]} cohort)"
+def print_student_list(names)
+  names.each_with_index do |name, index|
+      puts "#{index + 1}. #{name[:name]} (#{name[:cohort]} cohort)"
     end
 end
 
@@ -44,7 +45,43 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+
+
+def interactive_menu
+    
+    loop do
+        print_menu
+        selection = gets.chomp
+      case selection
+        when "1"
+        input_students
+        when "2"
+        show_students
+        when "9"
+        exit
+        else
+        puts "I don't know what you meant, try again"
+      end
+    end
+end
+
+def show_students
+    print_header
+    print_student_list(@students)
+    print_footer(@students)
+end
+
+# students = input_students
+# print_header
+# print(students)
+# print_footer(students)
+
+# print(students)
+
+interactive_menu
